8c7ebb46e93c60643f85b1a37812bccea2b71dc1
aAleNNx <127205564+aAleNNx@users.noreply.github.com>
2025-04-15 15:26:06 +0200

    Update JN.py

--- JN.py
+++ JN.py
@@ -1,11 +1,14 @@
-def KARTA(wartosc):
-    for i in range(10):
+def KARTA(wartosc, symbol):
+    for i in range(11):
         print("-", end="")
-    print(f"\n| {wartosc}      |")
-    for i in range(3):
-        print("|        |")
-    print(f"|      {wartosc} |")
-    for i in range(10):
+    print(f"\n| {wartosc}       |")
+    print("|         |")
+    print(f"|    {symbol}    |")
+    print("|         |")
+    print(f"|       {wartosc} |")
+    for i in range(11):
         print("-", end="")
+    print("\n")
         
-KARTA("J")
+KARTA("J", "♠")
+KARTA("Q","♦")
