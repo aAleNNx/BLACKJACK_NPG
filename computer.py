cfc558efa2f32b5ab30ac9a9f06f8b87e56ab970
bartlomiej-n <niezabitowskibartlomiej@gmail.com>
2025-06-05 17:42:55 +0200

    Wersja na 1 gracza

--- computer.py
+++ computer.py
@@ -10,7 +10,7 @@ class ComputerPlayer:
         self.standing = False
         self.busted = False
 
-    def add_card(self, card):
+    def add_card(self, card, deck, player):
         if self.next_card_multiplier != 1.0:
             card.multiplier = self.next_card_multiplier
             self.next_card_multiplier = 1.0
@@ -19,7 +19,10 @@ class ComputerPlayer:
             self.busted = True
 
         if getattr(card, 'is_special', False):
-            apply_special_effect(card, self, None, None)  # Deck i przeciwnik muszą być przekazane wyżej
+            apply_special_effect(card, self, deck, player)  # Deck i przeciwnik muszą być przekazane wyżej
+
+    def get_hand_value(self):
+        return calculate_hand_value(self.hand)
 
     def should_draw_card(self):
         value = calculate_hand_value(self.hand)
--- game.py
+++ game.py
@@ -4,9 +4,9 @@ from player import *
 from computer import *
 
 class Game:
-    def __init__(self, *players, opponent, deck_count = 1):
+    def __init__(self, player, opponent, deck_count = 1):
         self.deck = Deck(deck_count)
-        self.players = players
+        self.player = player
         self.opponent = opponent
     
     def run(self):
@@ -14,35 +14,51 @@ class Game:
             self.play_round()
         
     def play_round(self):
-        for player in self.players:
-            print(player.name)
-            player.add_card(self.deck.draw(), self.deck, self.opponent)
-            player.add_card(self.deck.draw(), self.deck, self.opponent)
-            player.show_hand()
-            print("Hand value:", player.get_hand_value(), "\n")
-            if player.get_hand_value() == 21:
-                print("BLACKJACK")
+        print(self.opponent.name)
+        self.opponent.add_card(self.deck.draw(), self.deck, self.player)
+        self.opponent.add_card(self.deck.draw(), self.deck, self.player)
+        print(self.opponent.hand[0])
+        print(f"{self.opponent.name}'s hand value:", self.opponent.hand[0].get_value(), "\n")
 
-            while player.get_hand_value() < 21:
-                print("1. Hit\n2. Stand")
-                choice = int(input())
-                if choice == 1:
-                    player.add_card(self.deck.draw(), self.deck, self.opponent)
-                    player.show_hand()
-                    print("Hand value:", player.get_hand_value(), "\n")
-                elif choice == 2:
-                    break
-                else:
-                    print("Invalid choice")
+        print(self.player.name)
+        self.player.add_card(self.deck.draw(), self.deck, self.opponent)
+        self.player.add_card(self.deck.draw(), self.deck, self.opponent)
+        self.player.show_hand()
+        print("Your hand value:", self.player.get_hand_value(), "\n")
 
-            if player.get_hand_value() > 21:
-                print("Bust!")
+        if self.player.get_hand_value() == 21:
+            print("BLACKJACK")
+
+        while self.player.get_hand_value() < 21:
+            print("1. Hit\n2. Stand")
+            choice = int(input())
+            if choice == 1:
+                self.player.add_card(self.deck.draw(), self.deck, self.opponent)
+                self.player.show_hand()
+                print("Your hand value:", self.player.get_hand_value(), "\n")
+            elif choice == 2:
+                break
             else:
-                # porównanie do ręki dealera albo innych graczy
-                pass
-            player.reset_hand()
+                print("Invalid choice")
+
+        if self.player.get_hand_value() > 21:
+            print(f"Bust! {self.opponent.name} wins!")
+        else:
+            print(f"{self.opponent.name}'s hand: \n")
+            self.opponent.show_hand()
+            while self.opponent.should_draw_card():
+                self.opponent.add_card(self.deck.draw(), self.deck, self.opponent)
+                print(self.opponent.hand[-1])
+            if self.player.get_hand_value() > self.opponent.get_hand_value():
+                print(f"{self.player.name} wins!")
+            elif self.player.get_hand_value() < self.opponent.get_hand_value():
+                print(f"{self.opponent.name} wins!")
+            else:
+                print("Draw.")
+            pass
+        self.player.reset_hand()
 
 player = Player("Leon")
-computer = ComputerPlayer("Krupier")
+computer = ComputerPlayer("Dealer")
 game = Game(player, opponent=computer)
 game.run()
